b79f9b1a23f4ed0e821e3bc4412361cba85ba61d
Cody <codygunton@gmail.com>
2025-04-27 02:42:44 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,11 @@
-#!/bin/bash
-set -e  # Exit on error
+sudo apt install build-essential libgmp-dev z3 pkg-config zlib1g-dev cmake curl texinfo
 
-# Install dependencies
-sudo apt-get update
-sudo apt-get install -y opam build-essential libgmp-dev z3 pkg-config zlib1g-dev cmake curl
+curl -L https://github.com/rems-project/sail/releases/download/0.19-linux-binary/sail.tar.gz -o sail.tar.gz
+tar -xzf sail.tar.gz
+rm sail.tar.gz
+export PATH=$PATH:$PWD/sail/bin
 
-# Initialize opam
-opam init -y --disable-sandboxing
-eval $(opam env)
-
-# Create ocaml switch and install dependencies
-opam switch create ocaml-base-compiler.4.06.1
-eval $(opam env)
-opam install -y sail
-
-# Clone and build sail-riscv
-git clone https://github.com/riscv/sail-riscv.git
+# git clone https://github.com/riscv/sail-riscv.git
 cd sail-riscv
 
-# Build the C emulator
-./build_c_emulator.sh
-
-# Make symbolic links
-cd ..
-sudo ln -sf $(pwd)/sail-riscv/c_emulator/riscv_sim_RV64 /usr/bin/riscv_sim_RV64
-sudo ln -sf $(pwd)/sail-riscv/c_emulator/riscv_sim_RV32 /usr/bin/riscv_sim_RV32
-
-echo "Setup complete! The sail-riscv emulator should now be installed."
+./build_simulators.sh
