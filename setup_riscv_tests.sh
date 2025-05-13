1745d5a6b79aa4aa7815b96130779efa6cfee9a0
Cody <codygunton@gmail.com>
2025-05-13 04:15:42 -0400

    Make a script; all tests pass

---
+++ setup_riscv_tests.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+set -eu
+
+export PATH=$PWD/toolchains/risc0/bin:$PATH
+
+git clone git@github.com:riscv-software-src/riscv-tests.git 
+cd riscv-tests 
+git submodule update --init --recursive 
+autoconf
+./configure CC=riscv32-unknown-elf-gcc --with-xlen=32 CFLAGS="-march=rv32im -nostdlib -mabi=ilp32"
+
+echo '
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#define __riscv_xlen 32
+#define TESTNUM x31
+
+#define RVTEST_RV32U                                                                               \
+  .macro init;                                                                                     \
+  .endm
+
+#define RVTEST_FAIL                                                                                \
+  fence;                                                                                           \
+  unimp
+#define RVTEST_PASS                                                                                \
+  li t0, 0;                                                                                        \
+  li a0, 0;                                                                                        \
+  li a1, 0x400;                                                                                    \
+  ecall
+
+#define RVTEST_CODE_BEGIN                                                                          \
+  .text;                                                                                           \
+  .globl _start;                                                                                   \
+  _start:                                                                                          \
+  .option push;                                                                                    \
+  .option norelax;                                                                                 \
+  la gp, __global_pointer$;                                                                        \
+  .option pop;
+
+#define RVTEST_CODE_END
+
+#define RVTEST_DATA_BEGIN .data
+#define RVTEST_DATA_END
+' > env/p/riscv_test.h
+
+echo '
+OUTPUT_ARCH( "riscv" )
+ENTRY(rvtest_entry_point)
+
+SECTIONS
+{
+  . = 0x80000000;
+  .text.init : { *(.text.init) }
+  . = ALIGN(0x1000);
+  .tohost : { *(.tohost) }
+  . = ALIGN(0x1000);
+  .text : { *(.text) }
+  . = ALIGN(0x1000);
+  .data : { 
+    __global_pointer$ = . + 0x800;
+    *(.data) 
+  }
+  .data.string : { *(.data.string)}
+  .bss : { *(.bss) }
+  _end = .;
+} 
+' > env/p/link.ld
+
+./configure CC=riscv32-unknown-elf-gcc --with-xlen=32 CFLAGS="-march=rv32im -nostdlib -mabi=ilp32"
+
+echo '
+#=======================================================================
+# Makefile for riscv-tests/isa
+#-----------------------------------------------------------------------
+
+XLEN ?= 64
+
+src_dir := .
+
+ifeq ($(XLEN),64)
+include $(src_dir)/rv64ui/Makefrag
+include $(src_dir)/rv64uc/Makefrag
+include $(src_dir)/rv64um/Makefrag
+include $(src_dir)/rv64ua/Makefrag
+include $(src_dir)/rv64uf/Makefrag
+include $(src_dir)/rv64ud/Makefrag
+include $(src_dir)/rv64uzfh/Makefrag
+include $(src_dir)/rv64uzba/Makefrag
+include $(src_dir)/rv64uzbb/Makefrag
+include $(src_dir)/rv64uzbc/Makefrag
+include $(src_dir)/rv64uzbs/Makefrag
+include $(src_dir)/rv64si/Makefrag
+include $(src_dir)/rv64ssvnapot/Makefrag
+include $(src_dir)/rv64mi/Makefrag
+include $(src_dir)/rv64mzicbo/Makefrag
+include $(src_dir)/hypervisor/Makefrag
+endif
+include $(src_dir)/rv32ui/Makefrag
+include $(src_dir)/rv32uc/Makefrag
+include $(src_dir)/rv32um/Makefrag
+include $(src_dir)/rv32ua/Makefrag
+include $(src_dir)/rv32uf/Makefrag
+include $(src_dir)/rv32ud/Makefrag
+include $(src_dir)/rv32uzfh/Makefrag
+include $(src_dir)/rv32uzba/Makefrag
+include $(src_dir)/rv32uzbb/Makefrag
+include $(src_dir)/rv32uzbc/Makefrag
+include $(src_dir)/rv32uzbs/Makefrag
+include $(src_dir)/rv32si/Makefrag
+include $(src_dir)/rv32mi/Makefrag
+
+default: all
+
+#--------------------------------------------------------------------
+# Build rules
+#--------------------------------------------------------------------
+
+RISCV_PREFIX ?= riscv$(XLEN)-unknown-elf-
+RISCV_GCC ?= $(RISCV_PREFIX)gcc
+RISCV_GCC_OPTS ?= -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles
+RISCV_OBJDUMP ?= $(RISCV_PREFIX)objdump --disassemble-all --disassemble-zeroes --section=.text --section=.text.startup --section=.text.init --section=.data
+RISCV_SIM ?= spike
+
+vpath %.S $(src_dir)
+
+#------------------------------------------------------------
+# Build assembly tests
+
+%.dump: %
+	$(RISCV_OBJDUMP) $< > $@
+
+%.out: %
+	$(RISCV_SIM) --isa=rv64gch_zfh_zicboz_svnapot_zicntr_zba_zbb_zbc_zbs --misaligned $< 2> $@
+
+%.out32: %
+	$(RISCV_SIM) --isa=rv32gc_zfh_zicboz_svnapot_zicntr_zba_zbb_zbc_zbs --misaligned $< 2> $@
+
+define compile_template
+
+$$($(1)_p_tests): $(1)-p-%: $(1)/%.S
+	$$(RISCV_GCC) $(2) $$(RISCV_GCC_OPTS) -I$(src_dir)/../env/p -I$(src_dir)/macros/scalar -T$(src_dir)/../env/p/link.ld $$< -o $$@
+$(1)_tests += $$($(1)_p_tests)
+
+$(1)_tests_dump = $$(addsuffix .dump, $$($(1)_tests))
+
+$(1): $$($(1)_tests_dump)
+
+.PHONY: $(1)
+
+COMPILER_SUPPORTS_$(1) := $$(shell $$(RISCV_GCC) $(2) -c -x c /dev/null -o /dev/null 2> /dev/null; echo $$$$?)
+
+ifeq ($$(COMPILER_SUPPORTS_$(1)),0)
+tests += $$($(1)_tests)
+endif
+
+endef
+
+$(eval $(call compile_template,rv32ui,-march=rv32g -mabi=ilp32))
+$(eval $(call compile_template,rv32um,-march=rv32g -mabi=ilp32))
+ifeq ($(XLEN),64)
+$(eval $(call compile_template,rv64ui,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64uc,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64um,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64ua,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64uf,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64ud,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64uzfh,-march=rv64g_zfh -mabi=lp64))
+$(eval $(call compile_template,rv64uzba,-march=rv64g_zba -mabi=lp64))
+$(eval $(call compile_template,rv64uzbb,-march=rv64g_zbb -mabi=lp64))
+$(eval $(call compile_template,rv64uzbc,-march=rv64g_zbc -mabi=lp64))
+$(eval $(call compile_template,rv64uzbs,-march=rv64g_zbs -mabi=lp64))
+$(eval $(call compile_template,rv64mzicbo,-march=rv64g_zicboz -mabi=lp64))
+$(eval $(call compile_template,rv64si,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64ssvnapot,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,rv64mi,-march=rv64g -mabi=lp64))
+$(eval $(call compile_template,hypervisor,-march=rv64gh -mabi=lp64))
+endif
+
+tests_dump = $(addsuffix .dump, $(tests))
+tests_hex = $(addsuffix .hex, $(tests))
+tests_out = $(addsuffix .out, $(filter rv64%,$(tests)))
+tests32_out = $(addsuffix .out32, $(filter rv32%,$(tests)))
+
+run: $(tests_out) $(tests32_out)
+
+junk += $(tests) $(tests_dump) $(tests_hex) $(tests_out) $(tests32_out)
+
+#------------------------------------------------------------
+# Default
+
+all: $(tests_dump)
+
+#------------------------------------------------------------
+# Clean up
+
+clean:
+	rm -rf $(junk)
+' > isa/Makefile
+
+make isa 
+
+mkdir isa/build
+rm -rf isa/*.dump
+for f in isa/rv32ui-p-*; do mv "$f" "isa/build/${f#isa/rv32ui-p-}"; done
+for f in isa/rv32um-p-*; do mv "$f" "isa/build/${f#isa/rv32um-p-}"; done
+
+echo "ISA tests built in isa/build"
+ls isa/build
