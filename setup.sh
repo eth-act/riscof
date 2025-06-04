b3f107d93fb9f5beb2aa45db3c8640008c49905b
Cody <codygunton@gmail.com>
2025-06-04 03:58:44 +0000

    small changes to setup script for repro

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 set -eu
-
-BASE_DIR=$PWD
 
 mkdir -p toolchains emulators
 
-# install gnu toolchain for building test ELFs if not present
+# Install RISC0-published rv32im toolchain if not already installed
 if [ ! -d "toolchains/risc0-riscv32im" ]; then
     echo "Downloading and installing RISC0 toolchain..."
     curl -L https://github.com/risc0/toolchain/releases/download/2024.01.05/riscv32im-linux-x86_64.tar.xz | tar -xJ -C toolchains/
@@ -15,7 +12,7 @@ else
     echo "RISC0 toolchain already installed, skipping..."
 fi
 
-# golden model risc-v simulator
+# Install "golden model" Sail RISC-V simulator if not already installed (the reference)
 if [ ! -d "emulators/sail-riscv" ]; then
     echo "Downloading and installing Sail model..."
     curl -L https://github.com/riscv/sail-riscv/releases/download/0.7/sail_riscv-Linux-x86_64.tar.gz | tar -xz -C emulators/
@@ -25,30 +22,28 @@ else
 fi
 
 
-# building risc0 (risc-v isa simulator)
+# Building r0vm, RISC0 emulator (the DUT)
 if [ ! -d "emulators/risc0" ]; then
-    echo "Cloning and building RISC0 emulator..."  
     git clone -b cg/riscof https://github.com/codygunton/risc0.git emulators/risc0
-    cd emulators/risc0
-    mkdir -p build
-    cargo build -p risc0-r0vm
-    cd $BASE_DIR
+    # debug build by default
+    echo "Building RISC0 emulator; this may take several minutes..."  
+    cargo build -p risc0-r0vm --manifest-path emulators/risc0/Cargo.toml --quiet
 else
     echo "RISC0 emulator already exists, skipping build..."
 fi
 
-# set up python environment
+# Set up python environment for RISCOF testing framework
 if [ ! -d ".venv" ]; then
     echo "Setting up Python virtual environment..."
     python3 -m venv .venv
     source .venv/bin/activate
     python3 -m pip install -r requirements.txt
-else
+elsed
     echo "Python virtual environment already exists, activating..."
     source .venv/bin/activate
 fi
 
-# setting up riscof testing framework with plugins
+# Get architectural tests if not already cloned
 if [ ! -d "riscv-arch-test" ]; then
     # Clone arch-test repository; pinning a version, but others may work too
     riscof arch-test --clone --get-version 3.9.1
