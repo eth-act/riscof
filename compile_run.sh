80aab7dc58c3d3c5b7ae37fb7a9a85c469dfaa49
Cody <codygunton@gmail.com>
2025-06-05 20:09:15 -0400

    start

--- compile_run.sh
+++ compile_run.sh
@@ -1,4 +1,4 @@
-rustup default nightly
+rustup default succinct
 BASE=$PWD
 PATH=$PATH:$BASE/toolchains/risc0-riscv32im/bin/
 
@@ -10,10 +10,10 @@ riscv32-unknown-elf-gcc \
   -nostdlib \
   -nostartfiles \
   -g \
-  -T plugins/risc0/env/link.ld \
-  -I plugins/risc0/env/ \
+  -T plugins/sp1/env/link.ld \
+  -I plugins/sp1/env/ \
   -I riscv-arch-test/riscv-test-suite/env \
-  riscv-arch-test/riscv-test-suite/rv32i_m/I/src/misalign1-jalr-01.S \
+  riscv-arch-test/riscv-test-suite/rv32i_m/I/src/add-01.S \
   -o my.elf \
   -DTEST_CASE_1=True \
   -DXLEN=32 \
@@ -26,18 +26,29 @@ riscv32-unknown-elf-objdump -D \
                             -j .data \
                             -j .riscv.attributes \
                             my.elf > $BASE/logs/my.elf.dump
-mv my.elf $BASE/emulators/risc0
-cd $BASE/emulators/risc0
+mv my.elf $BASE/emulators/sp1
+cd $BASE/emulators/sp1
 
-cargo build -p risc0-r0vm
+cargo build -p sp1-perf --bin sp1-perf-executor
 
-echo "Running test a few times to catch intermittent failures..."
-for i in {1..10}; do
-    echo "=== Run $i ==="
-    RUST_BACKTRACE=1 ./target/debug/r0vm --test-elf my.elf --signatures my.signatures
-    # RUST_BACKTRACE=1 RAYON_NUM_THREADS=1 RUSTFLAGS="--cfg single_threaded" ./target/debug/r0vm --test-elf my.elf --signatures my.signatures
-    echo ""
-done
+# Create empty stdin for SP1 (arch tests don't need input)
+python3 -c "
+import struct
+# Create minimal serialized SP1Stdin: empty buffer array + ptr=0 + empty proofs
+# This is a simplified bincode serialization of SP1Stdin::new()
+data = struct.pack('<Q', 0) + struct.pack('<Q', 0) + struct.pack('<Q', 0)  # empty vecs and ptr=0
+open('empty_stdin.bin', 'wb').write(data)
+"
+
+./target/debug/sp1-perf-executor --program my.elf --stdin empty_stdin.bin --executor-mode simple
+
+# echo "Running test a few times to catch intermittent failures..."
+# for i in {1..10}; do
+#     echo "=== Run $i ==="
+#     RUST_BACKTRACE=1 ./target/debug/r0vm --test-elf my.elf --signatures my.signatures
+#     # RUST_BACKTRACE=1 RAYON_NUM_THREADS=1 RUSTFLAGS="--cfg single_threaded" ./target/debug/r0vm --test-elf my.elf --signatures my.signatures
+#     echo ""
+# done
 
 # # Run with Valgrind multiple times to catch intermittent double free
 # echo "Running with Valgrind multiple times to catch intermittent memory corruption..."
